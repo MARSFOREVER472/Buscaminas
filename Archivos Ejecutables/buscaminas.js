5c9f44bfba8168204eb3774099368c74460361c1
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2025-09-08 12:13:08 -0300

    Editando la función del tablero...

--- Archivos Ejecutables/buscaminas.js
+++ Archivos Ejecutables/buscaminas.js
@@ -14,6 +14,11 @@ function paintBoard(rowNumbers, columnNumbers)
         for (let j = 0; j < columnNumbers; j++)
         {
             let newDiv = document.createElement("div");
+            newDiv.setAttribute("id", "i", + i + "_j" + j);
+            newDiv.dataset.row = i;
+            newDiv.dataset.column = j;
+            newDiv.addEventListener("contextmenu", tag); // EVENTO CON EL BOTÓN DERECHO DEL RATÓN.
+            newDiv.addEventListener("click", uncover); // EVENTO CON EL BOTÓN IZQUIERDO DEL RATÓN.
             board.appendChild(newDiv);
         }
     }
